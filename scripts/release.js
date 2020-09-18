b5daca214be72d27824d1cf25401779ff313246e
brendanatme <registrations@brendanat.me>
2020-09-18 09:15:37 -0400

    Fix release dir bug

--- scripts/release.js
+++ scripts/release.js
@@ -15,29 +15,20 @@ if (!bump) {
 
 execSync(`npm run prepare-release;`, err => console.error(err));
 
-console.log(`Removing tests...`);
-execSync(`rm -rf dist/tests`, err => console.error(err));
-
-console.log(`Removing examples...`);
-execSync(`rm -rf dist/examples`, err => console.error(err));
-
-console.log(`Removing docs...`);
-execSync(`rm -rf dist/docs`, err => console.error(err));
-
 console.log(`Bumping version by: ${bump}...`);
 execSync(`npm version ${bump}`, err => console.error(err));
 
 console.log('Copying package.json...');
-execSync(`cp package.json dist/package.json`, err => console.error(err));
+execSync(`cp package.json dist/lib/package.json`, err => console.error(err));
 
 console.log('Copying README.md...');
-execSync(`cp README.md dist/README.md`, err => console.error(err));
+execSync(`cp README.md dist/lib/README.md`, err => console.error(err));
 
 console.log('Copying .npmignore...');
-execSync(`cp .npmignore dist/.npmignore`, err => console.error(err));
+execSync(`cp .npmignore dist/lib/.npmignore`, err => console.error(err));
 
 console.log('Publishing to NPM...');
-execSync(`npm publish dist --access=public`, err => console.error(err));
+execSync(`npm publish dist/lib --access=public`, err => console.error(err));
 
 console.log('Pushing to Git...');
 execSync(`git push`, err => console.error(err));
